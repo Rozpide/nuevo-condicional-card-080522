dc6c09c8e6bf3c261a2e565309a48ee69e92e120
Rozpi <joserozpide@yahoo.es>
2022-05-07 17:47:26 +0000

    condicional-card-070522

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left, right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -28,20 +29,35 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  if (variables.name === null) variables.name = "Jose Luis";
+  if (variables.lastname === null) variables.lastname = "Rozpide";
+  if (variables.city === null) variables.city = "Pontevedra";
+  if (variables.country === null) variables.country = "España";
+  if (variables.role === null) variables.role = "Organizador";
+  let twitter = variables.twitter;
+  if (variables.twitter == null) twitter = "";
+  let github = `${variables.github}`;
+  if (variables.github == null) github = "";
+  let linkedin = variables.linkedin;
+  if (variables.linkedin == null) linkedin = "";
+  let instagram = variables.instagram;
+  if (variables.instagram == null) instagram = "";
+  if (variables.socialMediaPosition === right)
+    variables.socialMediaPosition = "Right";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <img src="src/20190726_155007 (2).jpg" class="photo" />
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
+          
         </div>
     `;
 }
